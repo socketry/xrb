e52166e0800d54f531a7efab097f033acbeca25f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-11-16 01:40:25 +1300

    Benchmark call vs yield.

---
+++ benchmark/call_vs_yield.rb
@@ -0,0 +1,51 @@
+
+require 'benchmark/ips'
+
+puts "Ruby #{RUBY_VERSION} at #{Time.now}"
+puts
+
+firstname = 'soundarapandian'
+middlename = 'rathinasamy'
+lastname = 'arumugam'
+
+def do_call(&block)
+	block.call
+end
+
+def do_yield(&block)
+	yield
+end
+
+def do_yield_without_block
+	yield
+end
+
+existing_block = proc{}
+
+Benchmark.ips do |x|
+	x.report("block.call") do |i|
+		buffer = String.new
+		
+		while (i -= 1) > 0
+			do_call(&existing_block)
+		end
+	end
+	
+	x.report("yield with block") do |i|
+		buffer = String.new
+		
+		while (i -= 1) > 0
+			do_yield(&existing_block)
+		end
+	end
+	
+	x.report("yield") do |i|
+		buffer = String.new
+		
+		while (i -= 1) > 0
+			do_yield_without_block(&existing_block)
+		end
+	end
+	
+	x.compare!
+end
