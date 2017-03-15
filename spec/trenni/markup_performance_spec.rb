532df1f3b5de40426267bcb42aec75ffb29fb294
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-03-16 00:35:01 +1300

    Refactor tag from utopia

---
+++ spec/trenni/markup_performance_spec.rb
@@ -0,0 +1,26 @@
+
+require 'benchmark/ips'
+require 'trenni/markup'
+
+RSpec.describe Trenni::Markup do
+	let(:code_string) {'javascript:if (foo < bar) {alert("Hello World")}'}
+	let(:general_string) {"a" * code_string.size}
+	
+	it "should be fast to parse large documents" do
+		Benchmark.ips do |x|
+			x.report("General String") do |times|
+				while (times -= 1) >= 0
+					Trenni::MarkupString(general_string)
+				end
+			end
+			
+			x.report("Code String") do |times|
+				while (times -= 1) >= 0
+					Trenni::MarkupString(code_string)
+				end
+			end
+			
+			x.compare!
+		end
+	end
+end
