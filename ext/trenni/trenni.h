32970ca7dfa891d41802121844dd7f9dfa5c7571
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-03-20 19:23:54 +1300

    Compatibility for older ruby

--- ext/trenni/trenni.h
+++ ext/trenni/trenni.h
@@ -12,6 +12,10 @@
 #define rb_str_cat_cstr rb_str_cat2
 #endif
 
+#ifndef RB_IMMEDIATE_P
+#define RB_IMMEDIATE_P IMMEDIATE_P
+#endif
+
 extern VALUE rb_Trenni, rb_Trenni_Markup, rb_Trenni_Tag, rb_Trenni_MarkupString, rb_Trenni_MarkupString_EMPTY, rb_Trenni_Native, rb_Trenni_ParseError;
 
 extern ID id_cdata, id_open_tag_begin, id_open_tag_end, id_attribute, id_close_tag, id_text, id_doctype, id_comment, id_instruction, id_read, id_expression, id_key_get, id_new, id_name, id_attributes, id_closed, id_to_s, id_is_a;
