148a639ca8ebca9063daf195da9083a20f36fe09
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-04-27 17:41:29 +1200

    Rework template tests.

--- test/xrb/template.rb
+++ test/xrb/template.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env rspec
 # frozen_string_literal: true
 
 # Released under the MIT License.
@@ -6,111 +5,57 @@
 
 require 'xrb/template'
 require 'xrb/parsers'
-require 'benchmark'
+
+class Formatter
+	def title_for(options)
+		options[:title]
+	end
+	
+	def value_for(options)
+		options[:value]
+	end
+	
+	def row(options = {}, &block)
+		XRB::Builder.fragment do |builder|
+			builder.inline(:dt) { builder.text title_for(options) }
+			
+			if block_given?
+				builder.inline(:dd) { builder.capture(self, &block) }
+			else
+				builder.inline(:dd) { builder.text value_for(options) }
+			end
+		end >> block
+	end
+end
 
 describe XRB::Template do
-	with 'large.xhtml' do
-		let(:template_path) {File.expand_path('.corpus/large.xhtml', __dir__)}
-		let(:template) {XRB::Template.load_file template_path}
-		let(:output) {template.to_string}
+	ROOT = File.expand_path(".template", __dir__)
+	
+	Dir.glob("*.xrb", base: ROOT) do |path|
+		name = File.basename(path, ".xrb")
 		
-		it "should parse xhtml template and produce identical output" do
-			expect{output}.not.to raise_exception
-			expect(output).to be == File.read(template_path)
+		with path, unique: name do
+			it "generates correct output" do
+				template_path = File.expand_path(path, ROOT)
+				output_path = File.expand_path("#{name}.txt", ROOT)
+				
+				template = subject.load_file(File.expand_path(path, ROOT))
+				output = template.to_string
+				
+				if File.exist?(output_path)
+					expect(template.to_string).to be == File.read(output_path)
+				else
+					File.write(output_path, output)
+				end
+			end
 		end
 	end
 	
-	with 'lines.xrb' do
-		let(:template_path) {File.expand_path('.corpus//lines.xrb', __dir__)}
-		let(:template) {XRB::Template.load_file template_path}
-		let(:code) {template.send(:code)}
+	it "should use default top-level binding if none provided" do
+		buffer = XRB::Buffer.new('#{Module.nesting.inspect}')
+		template = XRB::Template.new(buffer)
 		
-		it "should parse xhtml template and produce identical output" do
-			expect(code.lines[0]).to be =~ /apple/
-			expect(code.lines[5]).to be =~ /banana/
-		end
-	end
-	
-	with 'builder.xrb' do
-		let(:template_path) {File.expand_path('.corpus//builder.xrb', __dir__)}
-		let(:template) {XRB::Template.load_file template_path}
-		let(:code) {template.send(:code)}
-		
-		it "should capture and output the contents of the block" do
-			expect(template.to_string).to be == 'Hello World!'
-		end
-	end
-	
-	with 'capture.xrb' do
-		let(:capture_template) {XRB::Template.load_file File.expand_path('.corpus//capture.xrb', __dir__)}
-		
-		it "should be able to capture output" do
-			expect(capture_template.to_string).to be == '"TEST TEST TEST\n"'
-		end
-		
-		it "compiled template should match line numbers" do
-			code_lines = capture_template.send(:code).lines
-			
-			expect(code_lines.count).to be == 4
-			expect(code_lines[3]).to be(:include?, "inspect")
-		end
-	end
-	
-	with 'buffer.xrb' do
-		let(:buffer_template) {XRB::Template.load_file File.expand_path('.corpus//buffer.xrb', __dir__)}
-		
-		it "should be able to fetch output buffer" do
-			expect(buffer_template.to_string).to be == 'test'
-		end
-	end
-	
-	with 'nested.xrb' do
-		let(:nested_template) {XRB::Template.load_file File.expand_path('.corpus//nested.xrb', __dir__)}
-		
-		it "should be able to handle nested interpolations" do
-			expect(nested_template.to_string).to be == "Hello world!"
-		end
-	end
-	
-	with 'items' do
-		let(:items) {1..4}
-		
-		it "should process list of items" do
-			buffer = XRB::Buffer.new('<?r items.each do |item| ?>#{item}<?r end ?>')
-			template = XRB::Template.new(buffer)
-			
-			expect(template.to_string(self)).to be == "1234"
-		end
-		
-		it "should have correct indentation" do
-			buffer = XRB::Buffer.new("\t<?r items.each do |item| ?>\n\t\#{item}\n\t<?r end ?>\n")
-			template = XRB::Template.new(buffer)
-			
-			expect(template.to_string(self)).to be == "\t1\n\t2\n\t3\n\t4\n"
-		end
-	end
-	
-	with 'escaped.xrb' do
-		let(:escaped_template) {XRB::Template.load_file File.expand_path('.corpus//escaped.xrb', __dir__)}
-		
-		it "should have the same number of lines as input" do
-			expect(escaped_template.send(:code).lines.count).to be == 2
-		end
-		
-		it "should process escaped characters" do
-			expect(escaped_template.to_string).to be ==
-				"This\\nisn't one line.\n" +
-				"\\tIndentation is the best."
-		end
-	end
-	
-	with 'scope' do
-		it "should use default top-level binding if none provided" do
-			buffer = XRB::Buffer.new('#{Module.nesting.inspect}')
-			template = XRB::Template.new(buffer)
-			
-			expect(template.to_string(self)).to be == "[XRB]"
-		end
+		expect(template.to_string(self)).to be == "[XRB]"
 	end
 end
 
@@ -118,7 +63,7 @@ ATemplateParser = Sus::Shared("template parser") do
 	let(:delegate) {XRB::ParseDelegate.new}
 	
 	it "should fail to parse incomplete expression" do
-		buffer = XRB::Buffer.new('<img src="#{poi_product.photo.thumbnail_url" />')
+		buffer = XRB::Buffer.new('<img src="#{product.photo.thumbnail_url" />')
 		
 		expect{
 			subject.parse_template(buffer, delegate)
@@ -145,3 +90,4 @@ if defined? XRB::Native
 		it_behaves_like ATemplateParser
 	end
 end
+
