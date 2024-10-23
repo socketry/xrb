36fb738541c056592b08d44bf78c57d6ef5f5717
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-10-24 00:18:56 +1300

    Improve local development.

--- bake.rb
+++ bake.rb
@@ -7,7 +7,7 @@ def build
 	ext_path = File.expand_path("ext", __dir__)
 	
 	Dir.chdir(ext_path) do
-		system("ruby ./extconf.rb")
+		system("ruby ./extconf.rb") unless File.exist?("Makefile")
 		system("make")
 	end
 end
@@ -18,4 +18,14 @@ def clean
 	Dir.chdir(ext_path) do
 		system("make clean")
 	end
+end
+
+def template(path)
+	$LOAD_PATH << File.expand_path("ext", __dir__)
+	
+	require_relative "lib/xrb/template"
+	
+	template = XRB::Template.load_file(path)
+	
+	puts template.code
 end
